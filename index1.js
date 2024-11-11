7ae4d46deda9e3cb6e90a34717db3e2193d04da4
Nurzat Amirzhan <zhanna910411@gmail.com>
2024-11-11 17:00:58 +0300

    baqylau

---
+++ index1.js
@@ -0,0 +1,54 @@
+var num = prompt("1ден 12 ге шейін сан енгізіңіз")
+
+switch (num) {
+    case "3":
+    alert("ай 3, маусым:Көктем");
+    break;
+
+    case "4":
+    alert("ай 4, маусым:Көктем)");
+    break;
+
+    case "5":
+    alert("ай 5, маусым:Көктем");
+    break;
+
+    case "6":
+    alert("ай 6, маусым:Жаз");
+    break;
+
+    case "7":
+    alert("ай 7, маусым:Жаз");
+    break;
+
+    case "8":
+    alert("ай 8, маусым:Жаз");
+    break;
+
+    case "9":
+    alert("ай 9, маусым:Күз");
+    break;
+
+    case "10":
+    alert("ай 10, маусым:Күз");
+    break;
+
+    case "11":
+    alert("ай 11, маусым:Күз");
+    break;
+
+    case "12":
+    alert("ай 12, маусым:Қыс");
+    break;
+
+    case "1":
+    alert("ай 1, маусым:Қыс");
+    break;
+
+    case "2":
+    alert("ай 2, маусым:Қыс");
+    break;
+
+    
+    
+}
---
+++ index3.js
@@ -0,0 +1,16 @@
+var a = prompt("a санын енгізіңіз")
+var b = prompt("b санын енгізіңіз")
+
+if (a > b) {
+    alert("a үлкен b дан")
+    alert("a және b санының ортаншысы" + (a + b) /2)
+} else if(a < b) {
+    alert("a кіші b дан")
+    alert("a және b санының ортыншысы" + (a + b) /2)
+}
+else if(a = b) {
+    alert("a және b тең")
+}
+    
+
+    
